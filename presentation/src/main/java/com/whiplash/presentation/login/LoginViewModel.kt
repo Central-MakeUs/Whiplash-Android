e9789e7c79099954616682dbff6230aa838bf285
ask0908 <askdfjaslk@naver.com>
2025-06-30 23:35:30 +0900

    chore: LoginViewModel 주석 추가

--- presentation/src/main/java/com/whiplash/presentation/login/LoginViewModel.kt
+++ presentation/src/main/java/com/whiplash/presentation/login/LoginViewModel.kt
@@ -33,9 +33,14 @@ class LoginViewModel @Inject constructor(
     val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
 
     init {
+        // 이 뷰모델이 초기화될 때 현재 로그인 상태 확인
+        // 이미 로그인한 상태라면 그에 맞는 처리 수행
         checkCurrentUser()
     }
 
+    /**
+     * 현재 로그인된 유저가 있는지 확인
+     */
     private fun checkCurrentUser() {
         val currentUser = getCurrentUserUseCase()
         _uiState.update {
@@ -46,8 +51,20 @@ class LoginViewModel @Inject constructor(
         }
     }
 
+    /**
+     * 구글 로그인을 위한 인텐트를 가져온다
+     *
+     * 액티비티에서 런처를 통해 실행해 구글 로그인 시작
+     *
+     * @return 구글 로그인 인텐트
+     */
     fun getGoogleSignInIntent(): Intent = getGoogleSignInIntentUseCase() as Intent
 
+    /**
+     * 구글 로그인 결과 처리
+     *
+     * @param data 구글 로그인 결과(idToken 등)를 가진 인텐트
+     */
     fun handleSignInResult(data: Intent?) = viewModelScope.launch(Dispatchers.IO) {
         try {
             _uiState.update { it.copy(isLoading = true) }
@@ -77,6 +94,11 @@ class LoginViewModel @Inject constructor(
         }
     }
 
+    /**
+     * idToken으로 파이어베이스 인증 진행
+     *
+     * @param idToken 구글 로그인 후 받은 idToken
+     */
     private suspend fun signInWithGoogleToken(idToken: String) {
         try {
             signInWithGoogleUseCase.invoke(idToken)
@@ -109,6 +131,9 @@ class LoginViewModel @Inject constructor(
         }
     }
 
+    /**
+     * 구글 로그아웃
+     */
     fun signOut() = viewModelScope.launch(Dispatchers.IO) {
         try {
             _uiState.update { it.copy(isLoading = true) }
@@ -141,12 +166,22 @@ class LoginViewModel @Inject constructor(
         }
     }
 
+    /**
+     * 구글 로그인 에러 상태 초기화
+     */
     fun clearError() = _uiState.update { it.copy(error = null) }
 
     data class LoginUiState(
+        // 구글 로그인 진행 상태
         val isLoading: Boolean = false,
+
+        // 구글 로그인 후 받는 유저 정보(idToken 제외)
         val user: UserEntity? = null,
+
+        // 로그인 됐는가?
         val isSignIn: Boolean = false,
+
+        // 구글 로그인, 로그아웃 에러
         val error: String? = null
     )
 }
