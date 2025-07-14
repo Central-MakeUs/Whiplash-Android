ec0d3fb5f16ffe88fae6669522fd047e80734c67
ask0908 <askdfjaslk@naver.com>
2025-07-14 22:37:57 +0900

    feat: 알람 목록 화면 리사이클러뷰에 mock data 표시 처리, impl 클래스의 hilt 미적용 부분 수정

--- data/src/main/kotlin/com/whiplash/data/repository/alarm/AlarmRepositoryImpl.kt
+++ data/src/main/kotlin/com/whiplash/data/repository/alarm/AlarmRepositoryImpl.kt
@@ -1,15 +1,14 @@
 package com.whiplash.data.repository.alarm
 
-import com.whiplash.data.mapper.toEntity
 import com.whiplash.domain.entity.GetAlarmEntity
 import com.whiplash.domain.repository.alarm.AlarmRepository
-import com.whiplash.network.api.AlarmService
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class AlarmRepositoryImpl(
-    private val service: AlarmService
+class AlarmRepositoryImpl @Inject constructor(
+//    private val service: AlarmService // FIXME : api base url 추가되면 주석 해제
 ): AlarmRepository {
 
     /**
--- presentation/src/main/java/com/whiplash/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/whiplash/presentation/main/MainActivity.kt
@@ -2,12 +2,18 @@ package com.whiplash.presentation.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.whiplash.presentation.R
 import com.whiplash.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 /**
  * 알람 리사이클러뷰 표시 및 알람 등록 버튼, 상단에 알림 관련 문구 표시 등이 표시되는 메인 화면
@@ -16,6 +22,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var alarmListAdapter: AlarmListAdapter
+
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +38,33 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        setupRecyclerView()
+        observeMainViewModel()
+
         with(binding) {
             setHomeAlertPopupTexts()
+        }
+
+        mainViewModel.getAlarms()
+    }
+
+    private fun setupRecyclerView() {
+        alarmListAdapter = AlarmListAdapter()
+        binding.rvHomeAlarm.adapter = alarmListAdapter
+    }
+
+    private fun observeMainViewModel() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    mainViewModel.uiState.collect { state ->
+                        // 알람 목록 조회(현재는 mock data)
+                        val alarmList = state.alarmList
+                        Timber.d("## [알람 목록 조회] 액티비티에서 확인 : $state")
+                        alarmListAdapter.submitList(alarmList)
+                    }
+                }
+            }
         }
     }
 
