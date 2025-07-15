a68a67b467a47bdf611b9021d975278ab8c44d0c
ask0908 <askdfjaslk@naver.com>
2025-07-16 00:22:54 +0900

    refactor: HomeAlertPopup -> AlarmAlertView 이름 변경

--- presentation/src/main/java/com/whiplash/presentation/dialog/AlarmAlertView.kt
+++ presentation/src/main/java/com/whiplash/presentation/dialog/AlarmAlertView.kt
@@ -4,15 +4,18 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import com.whiplash.presentation.databinding.LayoutHomeAlertPopupBinding
+import com.whiplash.presentation.databinding.ViewHomeAlertBinding
 
-class AlarmAlertPopup @JvmOverloads constructor(
+/**
+ * 홈 화면 상단, 알람 설정 화면 하단에 표시되는 UI
+ */
+class AlarmAlertView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-    private val binding = LayoutHomeAlertPopupBinding.inflate(LayoutInflater.from(context), this, true)
+    private val binding = ViewHomeAlertBinding.inflate(LayoutInflater.from(context), this, true)
 
     fun setAlertTexts(
         firstText: String,
