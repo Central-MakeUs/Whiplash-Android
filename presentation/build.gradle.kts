167ff21b53076ec5196ffd8d6e55242738ad1043
ask0908 <askdfjaslk@naver.com>
2025-07-18 19:23:06 +0900

    refactor: number picker 라이브러리 의존성 사용 형태 수정

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -82,5 +82,6 @@ dependencies {
     // 네이버 지도
     implementation(libs.naver.map.sdk)
 
-    implementation("io.github.ShawnLin013:number-picker:2.4.13")
+    // 알람 설정 화면에서 사용하는 NumberPicker 라이브러리
+    implementation(libs.number.picker)
 }
