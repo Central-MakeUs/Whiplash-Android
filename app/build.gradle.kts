ccf74eb3dc5d70239bd2cb3af5c932d1f03670bf
ask0908 <askdfjaslk@naver.com>
2025-08-05 20:11:31 +0900

    project: 버전코드 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.whiplash.akuma"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
