955b40554f616342d9f05e699c0358839872031e
ask0908 <askdfjaslk@naver.com>
2025-08-07 18:47:59 +0900

    feat: 2번 온보딩 프래그먼트 UI 구현, 드로어블 추가

--- presentation/src/main/java/com/whiplash/presentation/onboarding/fragment/OnboardingSecondFragment.kt
+++ presentation/src/main/java/com/whiplash/presentation/onboarding/fragment/OnboardingSecondFragment.kt
@@ -1,7 +1,14 @@
 package com.whiplash.presentation.onboarding.fragment
 
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.whiplash.presentation.R
 import com.whiplash.presentation.databinding.FragmentOnboardingSecondBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +20,41 @@ class OnboardingSecondFragment : BaseOnboardingFragment<FragmentOnboardingSecond
         container: ViewGroup?
     ): FragmentOnboardingSecondBinding =
         FragmentOnboardingSecondBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupTitle()
+    }
+
+    private fun setupTitle() {
+        val fullText = "목표 장소, 목표 시간을"
+        val spannable = SpannableString(fullText)
+
+        val lemonColor = ContextCompat.getColor(requireContext(), R.color.lemon_400)
+
+        // '장소' 색상 적용
+        val placeStartIndex = fullText.indexOf("장소")
+        val placeEndIndex = placeStartIndex + "장소".length
+        spannable.setSpan(
+            ForegroundColorSpan(lemonColor),
+            placeStartIndex,
+            placeEndIndex,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        // '시간' 색상 적용
+        val timeStartIndex = fullText.indexOf("시간")
+        val timeEndIndex = timeStartIndex + "시간".length
+        spannable.setSpan(
+            ForegroundColorSpan(lemonColor),
+            timeStartIndex,
+            timeEndIndex,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.tvSecondOnboardingTitle.text = spannable
+    }
 
     companion object {
         fun newInstance() = OnboardingSecondFragment()
