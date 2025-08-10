5108f5b6d95a94b29b2e1a1a8a01cd5be4263256
ask0908 <askdfjaslk@naver.com>
2025-08-11 00:12:17 +0900

    refactor: usecase 모듈에 FCM 토큰 등록 usecase 등록

--- data/src/main/kotlin/com/whiplash/data/di/UseCaseModule.kt
+++ data/src/main/kotlin/com/whiplash/data/di/UseCaseModule.kt
@@ -11,6 +11,7 @@ import com.whiplash.domain.usecase.alarm.CreateAlarmOccurrenceUseCase
 import com.whiplash.domain.usecase.alarm.DeleteAlarmUseCase
 import com.whiplash.domain.usecase.alarm.GetAlarmsUseCase
 import com.whiplash.domain.usecase.alarm.TurnOffAlarmUseCase
+import com.whiplash.domain.usecase.auth.RegisterFcmTokenUseCase
 import com.whiplash.domain.usecase.auth.ReissueTokenUseCase
 import com.whiplash.domain.usecase.auth.SocialLoginUseCase
 import com.whiplash.domain.usecase.auth.SocialLogoutUseCase
@@ -59,6 +60,11 @@ object UseCaseModule {
     @Singleton
     fun provideSocialLogoutUseCase(repository: AuthRepository): SocialLogoutUseCase =
         SocialLogoutUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideRegisterFcmTokenUseCase(repository: AuthRepository): RegisterFcmTokenUseCase =
+        RegisterFcmTokenUseCase(repository)
 
     @Provides
     @Singleton
